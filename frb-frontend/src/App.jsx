41698e8ae128f82200b3abda769f1a7128086f84
Abvethanorm <norman.pegues@gmail.com>
2023-09-05 21:16:20 -0400

    changed api to DeepAI api

--- frb-frontend/src/App.jsx
+++ frb-frontend/src/App.jsx
@@ -5,11 +5,10 @@ import Nav from "./components/navigation/Nav";
 import Search from "./components/search/Search";
 import { createContext, useState } from "react";
 export const imgUrlContext = createContext();
-export const faceBox = createContext();
 
 function App() {
   const [imgUrl, setImgUrl] = useState("");
-  const [box, setBox] = useState({});
+
   return (
     <>
       <imgUrlContext.Provider value={[imgUrl, setImgUrl]}>
--- frb-frontend/src/components/search/Search.jsx
+++ frb-frontend/src/components/search/Search.jsx
@@ -1,83 +1,29 @@
-import "./search.css";
-import { useContext, useState } from "react";
+import React, { useState, useContext } from "react";
 import { imgUrlContext } from "../../App";
+import deepai from "deepai";
+
+import "./search.css";
 
 function Search() {
-  const [imgUrl, setImgUrl] = useContext(imgUrlContext);
   const [inputValue, setInputValue] = useState("");
-
+  const [imgUrl, setImgUrl] = useContext(imgUrlContext);
   const handleSearch = (e) => {
     const searchInput = e.target.value;
     setInputValue(searchInput);
   };
 
-  const flockEm = () => {
-    const findFace = (response) => {
-      response
-        .json()
-        .then((data) => {
-          console.log(data); // Log the entire JSON response
-          if (
-            data.outputs &&
-            data.outputs.length > 0 &&
-            data.outputs[0].data.regions
-          ) {
-            const clarifaiFace =
-              data.outputs[0].data.regions[0].region_info.bounding_box;
-            const image = document.getElementById("inputimage");
-            const width = Number(image.width);
-            const height = Number(image.height);
-            console.log(width, height);
-          } else {
-            console.log("No face data found in the response.");
-          }
-        })
-        .catch((error) => console.log("Error parsing JSON response:", error));
-    };
+  const flockEm = async () => {
+    deepai.setApiKey("9c12eef8-da61-40c5-ad94-0ce775da558a");
 
-    const PAT = "a7a9dd5f386e4fa3a93ee1694c9c2fb5";
-    const USER_ID = "epj7ya83au2p";
-    const APP_ID = "my-first-application-fboe3e";
-    const MODEL_ID = "face-detection";
-    const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
-
-    // Change the way you define IMAGE_URL
-    const IMAGE_URL = imgUrl; // Remove the curly braces
-
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: IMAGE_URL,
-            },
-          },
-        },
-      ],
-    });
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json", // Change "Accept" to "Content-Type"
-        Authorization: "Key " + PAT,
-      },
-      body: raw,
-    };
-
-    // Correct the URL format
-    fetch(
-      `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`,
-      requestOptions
-    )
-      .then((response) => findFace(response))
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
-    setImgUrl(inputValue);
+    try {
+      let resp = await deepai.callStandardApi("text2img", {
+        text: inputValue,
+      });
+      setImgUrl(resp.output_url);
+      console.log(resp);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
