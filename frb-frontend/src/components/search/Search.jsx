22e1684c6cce0535725facf39a9a8a6110315f05
Abvethanorm <norman.pegues@gmail.com>
2023-09-08 11:15:01 -0400

    added loading page to search component

--- frb-frontend/src/components/search/Search.jsx
+++ frb-frontend/src/components/search/Search.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import deepai from "deepai";
 import "./search.css";
 import "./recon.css";
+import Lottie from "lottie-react";
+import animationData from "../../assets/loading.json";
 
 function Search() {
   const [inputValue, setInputValue] = useState("");
@@ -15,6 +17,7 @@ function Search() {
     deepai.setApiKey("9c12eef8-da61-40c5-ad94-0ce775da558a");
 
     try {
+      setIsLoading(true);
       let resp = await deepai.callStandardApi("text2img", {
         text: inputValue,
       });
@@ -22,30 +25,52 @@ function Search() {
       console.log(resp);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="search__container container">
-      <h3 className="search__title">Who we trackin?</h3>
-      <div className="input__container container">
-        <input
-          onChange={handleSearch}
-          className="search__input"
-          type="text"
-          placeholder="  run ricky yeah we runnin !"
-        />
-        <button onClick={flockEm} className="search__btn">
-          Flock em
-        </button>
-      </div>
-      <div className="recon__container container">
-        <img id="inputimage" src={imgUrl} alt="Face Recognition" />
-      </div>
-      <div className="recon__btn__container">
-        <button>Download</button>
-        <button>Save</button>
-      </div>
+      {isLoading ? null : (
+        <>
+          <h3 className="search__title">Who we trackin?</h3>
+          <div className="input__container container">
+            <input
+              onChange={handleSearch}
+              className="search__input"
+              type="text"
+              placeholder="  run ricky yeah we runnin !"
+            />
+            <button onClick={flockEm} className="search__btn">
+              Flock em
+            </button>
+          </div>
+        </>
+      )}
+      {isLoading ? (
+        <div className="loading__container">
+          <Lottie animationData={animationData} style={{ width: 400 }} />
+          <div className="loading__message__container">
+            <h4 className="loading__message">We cookin</h4>
+            <span className="loading__dots">.</span>
+            <span className="loading__dots">.</span>
+            <span className="loading__dots">.</span>
+          </div>
+        </div>
+      ) : imgUrl ? (
+        <div>
+          <div className="recon__container container">
+            <img id="inputimage" src={imgUrl} alt="Face Recognition" />
+          </div>
+          <div className="recon__btn__container">
+            <button className="recon__btn">Download</button>
+            <button className="recon__btn">Save</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
